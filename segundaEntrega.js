1ebadf4e9fcbc12eaa210ad3776eb4638afc5751
Luis C. Sepulveda <luiscsepulveda.dev@gmail.com>
2023-03-07 07:08:00 -0300

    update product no funciona

--- segundaEntrega.js
+++ segundaEntrega.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-export default class ProductManager{
+export default class ProductManager {
     constructor() {
         this.products = [];
         this.path = "./files/products.json";
@@ -8,16 +8,15 @@ export default class ProductManager{
 
     getProducts = async () => {
 
-        
-    if(fs.existsSync(this.path)) {  
-        const data = await fs.promises.readFile(this.path, "utf-8");
 
-        const result = JSON.parse(data);
-        console.log(result);
-        return result;
-    } else{
-        return [];
-    }
+        if (fs.existsSync(this.path)) {
+            const data = await fs.promises.readFile(this.path, "utf-8");
+
+            const result = JSON.parse(data);
+            return result;
+        } else {
+            return [];
+        }
 
     };
 
@@ -25,11 +24,17 @@ export default class ProductManager{
     addProduct = async (title, description, price, thumbnail, code, stock) => {
 
         const products = await this.getProducts();
-    
-        if(!title || !description || !price || !thumbnail || !code || !stock) {
-            console.log ("Error: All fields are mandatory")
-            return; 
-        };  
+
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log("Error: All fields are mandatory")
+            return;
+        };
+
+        let sameProduct = products.find((element) => element.code === code);
+        if (sameProduct) {
+            return `Already exist a product with this code ${code}`
+        }
+
 
         const product = {
             id: products.length + 1,
@@ -41,8 +46,22 @@ export default class ProductManager{
             stock: stock,
         }
 
-        const lastProductId = this.products.length > 0 ? this.products[this.products.length - 1].id : 0;
-            const newProductId = lastProductId + 1;
+    let maxId = 0;
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id > maxId) {
+                maxId = products[i].id;
+            }
+        }
+        products.id = maxId + 1;
+
+        /* if(products.length === 0){
+            product.id = 1;
+        }else{
+            product.id = products[products.length -1].id +1
+        } */
+
+        /* const lastProductId = this.products.length > 0 ? this.products[this.products.length - 1].id : 0;
+            const newProductId = lastProductId + 1; */
 
         products.push(product);
         await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
@@ -51,13 +70,15 @@ export default class ProductManager{
     }
 
     getProductsById = async (id) => {
-        if(fs.existsSync(this.path)) {
+        if (fs.existsSync(this.path)) {
             let result = await this.getProducts();
 
-            let productId = result.find(id  => result.id === id);
-            if(!productId) {
-                console.log(`Error: Product with ID ${'Id'} was not found` );
-            }else{
+            /* let productId = result.find(id  => result.id === id); */
+            let productId = result.find((res) => res.id === id);
+
+            if (!productId) {
+                console.log(`Error: Product with ID ${'Id'} was not found`);
+            } else {
                 return productId;
             }
 
@@ -67,18 +88,18 @@ export default class ProductManager{
 
     }
 
-    
-    updateProduct = async (id,code,title,description,price,thumbnail,stock) => {
+
+    updateProduct = async (id, code, title, description, price, thumbnail, stock) => {
 
         const products = await this.getProducts();
 
-        if(products == "error"){
+        if (products == "error") {
             return "this file is empty"
         }
 
-        let searchProduct = products.find((product) => product.id === id)
+        let searchProduct = products.find((product) => product.id !== id)
 
-        if(searchProduct != undefined) {
+        if (searchProduct === undefined) {
             const changeProduct = products.filter((product) => product.id === id);
 
             const updatedProduct = {
@@ -96,22 +117,22 @@ export default class ProductManager{
 
             await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
             return "product has been updated";
-        } else{
+        } else {
             return `Product with id ${id} cannot be updated because it does not exist in this list`
         }
     }
 
     deleteProducts = async (id) => {
-        
+
         const products = await this.getProducts()
 
         let existingProduct = products.find((product) => product.id === id);
-        if(existingProduct) {
+        if (existingProduct) {
             const feature = products.filter((del) => del.id != id);
 
             await fs.promises.writeFile(this.path, JSON.stringify(feature, null, "\t"))
             return "product has been deleted"
-        }else{
+        } else {
             return `Product with id ${id} cannot be deleted because it does not exist in this list`
         }
 
@@ -119,4 +140,3 @@ export default class ProductManager{
 
 
 }
-
